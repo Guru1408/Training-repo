2383fc82d7e31d273c139088756cd3009ced4722
Your Name <you@example.com>
2024-08-17 20:27:23 +0530

    odd and even numbers

--- src/com/training/Class4.java
+++ src/com/training/Class4.java
@@ -7,34 +7,34 @@ public class Class4 {
 
 		System.out.println("Numbers from 1 to 100 (Even and Odd):");
 
-        // Using a for loop to iterate from 1 to 100
+        
         for (int i = 1; i <= 100; i++) {
-            // Check if the number is even
+           
             if (i % 2 == 0) {
-                // Print the even number
+                // Printing the even numbers
                 System.out.println(i + " is even");
             } else {
-                // Print the odd number
+                // Printing the odd numbers
                 System.out.println(i + " is odd");
             }
         }
         
         
-        int i = 1; // Initialize counter variable
+        // here taken i = 1
+        int i = 1;
 
         System.out.println("Numbers from 1 to 100 (Even and Odd):");
 
-        // Using a while loop to iterate from 1 to 100
         while (i <= 100) {
-            // Check if the number is even
+           
             if (i % 2 == 0) {
-                // Print the even number
+                // Printing the even numbers
                 System.out.println(i + " is even");
             } else {
-                // Print the odd number
+                // Printing the odd numbers
                 System.out.println(i + " is odd");
             }
-            i++; // Increment the counter variable
+            i++;
         }
         
       }
